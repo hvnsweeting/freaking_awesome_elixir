cea65872f6722d0a9c45f77e53ad0362a062576e
Viet Hung Nguyen <hvn@familug.org>
2020-03-05 22:17:05 +0700

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule FreakingAwesomeElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -21,6 +23,8 @@ defmodule FreakingAwesomeElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: [:test]},
       # {:jason, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
