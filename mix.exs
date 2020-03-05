9d8802eea9d9e2c3eb0fb6cfc4f8e328041fd675
Viet Hung Nguyen <hvn@familug.org>
2020-03-05 23:07:32 +0700

    Format

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule FreakingAwesomeElixir.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -24,7 +29,7 @@ defmodule FreakingAwesomeElixir.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
-      {:excoveralls, "~> 0.10", only: [:test]},
+      {:excoveralls, "~> 0.10", only: [:test]}
       # {:jason, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
